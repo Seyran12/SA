a8ef003df3c133a67b553ad24767128b4b97a714
Seyran Abdullayev <s.abdullayevjb@gmail.com>
2017-05-25 01:36:29 -0700

    pmd correction1

--- src/main/java/core/CPD.java
+++ src/main/java/core/CPD.java
@@ -17,17 +17,17 @@ public class CPD {
     }
 
     // BigIntegerInstantiation
-    public void BigInteger() {
-        BigInteger bi1 = new BigInteger("1"); 	// reference BigInteger.ONE
-        // instead
-        BigInteger bi2 = new BigInteger("0"); 	// reference BigInteger.ZERO
-        // instead
-        BigInteger bi3 = new BigInteger("0.0"); // reference BigInteger.ZERO
-        // instead
-        BigInteger bi4;
-        bi4 = new BigInteger("0"); 				// reference BigInteger.ZERO instead
-        System.out.println(bi1 + ", " + bi2 + ", " + bi3 + ", " + bi4);
-    }
+//    public void BigInteger() {
+//        BigInteger bi1 = new BigInteger("1"); 	// reference BigInteger.ONE
+//        // instead
+//        BigInteger bi2 = new BigInteger("0"); 	// reference BigInteger.ZERO
+//        // instead
+//        BigInteger bi3 = new BigInteger("0.0"); // reference BigInteger.ZERO
+//        // instead
+//        BigInteger bi4;
+//        bi4 = new BigInteger("0"); 				// reference BigInteger.ZERO instead
+//        System.out.println(bi1 + ", " + bi2 + ", " + bi3 + ", " + bi4);
+//    }
 
 
     // JumbledIncrementer
@@ -35,7 +35,7 @@ public class CPD {
         public void foo() {
             for (int i = 0; i < 10; i++) { // only references 'i'
                 for (int k = 0; k < 20; i++) { // references both 'i' and 'k'
-                    System.out.println("Hello");
+                    System.out.println("Hello");pwd
                 }
             }
         }
