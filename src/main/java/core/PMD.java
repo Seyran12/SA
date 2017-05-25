2ddedfe9aeb8b5c8cf885a1a65d19c9b1d6376fa
Seyran Abdullayev <s.abdullayevjb@gmail.com>
2017-05-25 09:18:06 -0700

    5

--- src/main/java/core/PMD.java
+++ src/main/java/core/PMD.java
@@ -12,58 +12,58 @@ public class PMD {
             int value = 0;
             int i = 0;
             String s = null;
-            value += (s.replaceAll("\\s", "").toLowerCase().charAt(i));
+            //      value += (s.replaceAll("\\s", "").toLowerCase().charAt(i));
 
         }
     }
 
     // BigIntegerInstantiation
     public void BigInteger() {
-       // BigInteger bi1 = new BigInteger("1"); 	// reference BigInteger.ONE
+        // BigInteger bi1 = new BigInteger("1"); 	// reference BigInteger.ONE
         // instead
-     //   BigInteger bi2 = new BigInteger("0"); 	// reference BigInteger.ZERO
+        //   BigInteger bi2 = new BigInteger("0"); 	// reference BigInteger.ZERO
         // instead
         BigInteger bi3 = new BigInteger("0.0"); // reference BigInteger.ZERO
         // instead
-     //   BigInteger bi4;
-       // bi4 = new BigInteger("0"); 				// reference BigInteger.ZERO instead
-        System.out.println( bi3 );
+        //   BigInteger bi4;
+        // bi4 = new BigInteger("0"); 				// reference BigInteger.ZERO instead
+        System.out.println(bi3);
     }
 
 
     // JumbledIncrementer
     public class JumbledIncrementerRule1 {
         public void foo() {
-            for (int i = 0; i < 10; i++) { // only references 'i'
-                for (int k = 0; k < 20; i++) { // references both 'i' and 'k'
-                    System.out.println("Hello");
-                }
-            }
+//            for (int i = 0; i < 10; i++) { // only references 'i'
+//                for (int k = 0; k < 20; i++) { // references both 'i' and 'k'
+//                    System.out.println("Hello");
+//                }
         }
     }
+
 
     // ForLoopShouldBeWhileLoop
 
     public class Foo {
         void bar() {
-            for (; true;)
-                ; // No Initialization or Update part, may as well be: while
+            //          for (; true;)
+            ; // No Initialization or Update part, may as well be: while
             // (true)
         }
     }
 
     // ReturnFromFinallyBlock
-    public class Bar {
-        public String foo() {
-            try {
-                throw new Exception("My Exception");
-            } catch (Exception e) {
-                throw e;
-            } finally {
-                return "ok"; // return not recommended here
-            }
-        }
-    }
+//    public void class Bar {
+//        public String foo() {
+//            try {
+//                throw new Exception("My Exception");
+//            } catch (Exception e) {
+//                throw e;
+//         //   } finally {
+//          //      return "ok"; // return not recommended here
+//            //}
+//        }
+//    }
 
     // UnconditionalIfStatement
 //    public class Foo2 {
@@ -94,12 +94,13 @@ public class PMD {
     public class LongVariable {
         int reallyLongIntName = -3; // VIOLATION - Field
 
-        public void main(String argumentsList[]) { 	// VIOLATION - Formal
-            int otherReallyLongName = -5; 			// VIOLATION - Local
-            for (int interestingIntIndex = 0; 		// VIOLATION - For
+        public void main(String argumentsList[]) {    // VIOLATION - Formal
+            //       int otherReallyLongName = -5; 			// VIOLATION - Local
+            for (int interestingIntIndex = 0;        // VIOLATION - For
                  interestingIntIndex < 10; interestingIntIndex++) {
             }
         }
 
     }
+
 }
